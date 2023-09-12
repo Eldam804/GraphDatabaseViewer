af309d7817ddc37367703b3f705dd5a9e36611a9
Eldam804 <adam.kleinedler@gmail.com>
2023-09-12 19:10:34 +0200

    Added differentiator for successful/failed queries

--- src/app/Nodes/queries/queries.component.ts
+++ src/app/Nodes/queries/queries.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { DriverService } from 'src/app/Neo4j/Database/driver.service';
+import { of } from 'rxjs';
+import { error } from 'neo4j-driver';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-queries',
@@ -9,15 +12,36 @@ import { DriverService } from 'src/app/Neo4j/Database/driver.service';
 })
 export class QueriesComponent {
   public query?: String;
-  public queries: String[] = [];
+  public queries: any[] = [];
   constructor(private neo4jDriver: DriverService) {
   }
 
   sendQuery() {
     if(this.query){
-      this.queries.push(this.query);
-      this.neo4jDriver.sendQuery(this.query);
-      this.query = "";
+      this.neo4jDriver.sendQuery(this.query)
+        .subscribe((result: any) => {
+            // Success block
+            const queryObj = {
+              query: this.query,
+              message: 'Success'
+            };
+            this.queries.push(queryObj);
+            console.debug(this.queries);
+            this.query = "";
+          },
+          (error) => {
+            // Error block
+            console.error(error);
+            const queryObj = {
+              query: this.query,
+              message: 'Error'
+            };
+            this.queries.push(queryObj);
+            console.debug(this.queries);
+            this.query = "";
+          }
+        );
+      
     }
   }
 
